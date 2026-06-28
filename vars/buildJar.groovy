2350c1e2b3ffa768f5f309eec6eb339574c4a7ee
Jeremy <343220868@qq.com>
2026-06-28 19:33:32 +0930

    update buildJar.groovy of the parameter

--- vars/buildJar.groovy
+++ vars/buildJar.groovy
@@ -1,6 +1,6 @@
 #!/usr/bin/env groovy
 
-def call(String BRANCH_NAME) {
-    echo "building the application for $BRANCH_NAME branch"
+def call(String branchName) {
+    echo "building the application for $branchName branch"
     sh 'mvn package'
 }
