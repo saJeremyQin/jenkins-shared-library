8944d80c46a53c31470af93d5b3556ae954e0888
Jeremy <343220868@qq.com>
2026-06-28 19:30:44 +0930

    create Docker.groovy, java class

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,10 +1,8 @@
 #!/usr/bin/env groovy
 
+import com.mooji.Docker
+
 def call(String imageName) {
-    echo "building the docker image of version .."
-    withCredentials([usernamePassword(credentialsId: 'docker-hub-credentials', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh "docker build -t $imageName ."
-        sh 'echo $PASS | docker login -u $USER --password-stdin'
-        sh "docker push $imageName"
-    }
+    def docker = new Docker(this)
+    docker.publishImage(imageName)
 }
